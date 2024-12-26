31e1e4c43289c933eb5d27bcdcdb7c9efc3a0352
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-12-26 01:13:50 -0500

    ..

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def home():
     return "La API está funcionando. Accede a las rutas /api/provincias o /api/delitos."
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=8000)
+    serve(app, host='0.0.0.0', port=10000)
 
